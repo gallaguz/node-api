d3f70303979309eeb057e36ba0eee1fb13919bb9
gallaguz <Perec8873732>
2022-05-02 04:41:24 +0300

    user controller

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
 import express, { Express } from 'express';
-import { userRouter } from './router';
 import { Server } from 'http';
 import { LoggerService } from './logger';
+import { UserController } from './user/user.controller';
 
 export class App {
   app: Express;
   server: Server;
   port: number;
   logger: LoggerService;
+  userController: UserController;
 
-  constructor(logger: LoggerService) {
+  constructor(logger: LoggerService, userController: UserController) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
+    this.userController = userController;
   }
 
   useRoutes() {
-    this.app.use('/users', userRouter);
+    this.app.use('/users', this.userController.router);
   }
 
   public async init() {
---
+++ src/common/base.route.interface.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response, Router } from 'express';
+
+export interface IBaseRoute {
+  path: string;
+  func: (req: Request, res: Response, next: NextFunction) => void;
+  method: keyof Pick<
+    Router,
+    'get' | 'post' | 'delete' | 'patch' | 'put' | 'options'
+  >;
+}
