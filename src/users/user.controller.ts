04e4b6dca3c16fea92dff9f209bf8d125b2cacd8
gallaguz <gallaguz@gmail.com>
2022-07-11 11:01:12 +0300

    debug

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -17,7 +17,6 @@ export class UserController extends BaseController implements IUserController {
     }
 
     login(req: Request, res: Response, next: NextFunction): void {
-        // this.ok(res, 'login');
         next(new HttpError(401, 'Auth error', 'login'));
     }
 
