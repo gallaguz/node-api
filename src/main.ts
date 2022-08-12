22ee35d557a5fc0df232447077e464b0bb1286ff
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-12 11:58:51 +0300

    main.ts: fix import, move "appBindings" to separate file "./src/inversify/inversify.config.ts"

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,18 @@
 if (!process.env.IS_TS_NODE) require('module-alias/register');
 
 import 'reflect-metadata';
-import { Container, ContainerModule, interfaces } from 'inversify';
+
+import { Container } from 'inversify';
 
 import { App } from '@app/app';
-import { IConfigService, ConfigService } from '@app/config';
-import { PrismaService } from '@app/database';
-import { ExceptionFilter, IExceptionFilter } from '@app/errors';
-import { ILogger, LoggerService } from '@app/logger';
+import { appBindings } from '@app/inversify';
+import { LoggerService } from '@app/logger';
 import { TYPES } from '@app/types';
-import {
-    UserController,
-    IUserController,
-    IUserService,
-    UserService,
-    UserRepository,
-    IUserRepository,
-} from '@app/users';
 
 export interface IBootstrapReturn {
     appContainer: Container;
     app: App;
 }
-
-export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-    bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
-    bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
-    bind<IUserController>(TYPES.UserController).to(UserController);
-    bind<IUserService>(TYPES.UserService).to(UserService);
-    bind<PrismaService>(TYPES.PrismaService)
-        .to(PrismaService)
-        .inSingletonScope();
-    bind<IConfigService>(TYPES.ConfigService)
-        .to(ConfigService)
-        .inSingletonScope();
-    bind<IUserRepository>(TYPES.UserRepository)
-        .to(UserRepository)
-        .inSingletonScope();
-    bind<App>(TYPES.Application).to(App);
-});
 
 async function bootstrap(): Promise<IBootstrapReturn> {
     const appContainer = new Container();
