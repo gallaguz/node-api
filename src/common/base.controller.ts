410560f3d1c5e2ce8c0d6ca1468b81d1b2890e23
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-13 02:27:24 +0300

    BaseController.bindRoutes: add logging

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -33,8 +33,9 @@ export abstract class BaseController {
     }
 
     protected bindRoutes(routes: IRouteInterface[]): void {
+        this.logger.log(`[Routes Binding] - ${this.constructor.name}:`);
         for (const route of routes) {
-            this.logger.log(`[${route.method}] ${route.path}`);
+            this.logger.log(` - [${route.method}] ${route.path}`);
 
             const middleware = route.middlewares?.map((m) => m.execute.bind(m));
             const handler = route.func.bind(this);
