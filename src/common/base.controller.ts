a8768cd9bec13eae165add3713e58add64d70a4d
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-12 08:22:48 +0300

    base.controller: fix imports and interface IControllerRoute => IRouteInterface

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -2,14 +2,14 @@ import 'reflect-metadata';
 import { Response, Router } from 'express';
 import { injectable } from 'inversify';
 
-import { ILogger } from '../logger';
-import { ExpressReturnType, IControllerRoute } from './route.interface';
+import { ExpressReturnType, IRouteInterface } from '@app/common';
+import { ILogger } from '@app/logger';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 @injectable()
 export abstract class BaseController {
-    private readonly _router: Router;
+    protected readonly _router: Router;
 
     protected constructor(private logger: ILogger) {
         this._router = Router();
@@ -32,13 +32,14 @@ export abstract class BaseController {
         return res.sendStatus(201);
     }
 
-    protected bindRoutes(routes: IControllerRoute[]): void {
+    protected bindRoutes(routes: IRouteInterface[]): void {
         for (const route of routes) {
             this.logger.log(`[${route.method}] ${route.path}`);
 
             const middleware = route.middlewares?.map((m) => m.execute.bind(m));
             const handler = route.func.bind(this);
             const pipeline = middleware ? [...middleware, handler] : handler;
+
             this.router[route.method](route.path, pipeline);
         }
     }
