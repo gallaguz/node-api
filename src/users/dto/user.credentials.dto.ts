9c739e46aa075feda07546a671b324a13491b0c7
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-12 09:07:26 +0300

    fix dto's

--- src/users/dto/user.credentials.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString } from 'class-validator';
-
-export class UserCredentialsDto {
-    @IsEmail({}, { message: 'Wrong email' })
-    email: string;
-
-    @IsString({ message: 'Wrong password' })
-    password: string;
-}
