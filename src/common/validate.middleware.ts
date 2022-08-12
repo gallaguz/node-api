ee66891caf078051d096d63e9aa450fc4e6eba18
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-12 09:17:03 +0300

    fix imports

--- src/common/validate.middleware.ts
+++ src/common/validate.middleware.ts
@@ -2,7 +2,7 @@ import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { NextFunction, Request, Response } from 'express';
 
-import { IMiddleware } from './middleware.interface';
+import { IMiddleware } from '@app/common';
 
 export class ValidateMiddleware implements IMiddleware {
     constructor(private classToValidate: ClassConstructor<object>) {}
