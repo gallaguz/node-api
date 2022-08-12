7df371bfdce60280849f53f4f2cca4f7e0425694
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-12 11:45:26 +0300

    add HttpErrorInterface for HttpError class

--- src/errors/http.error.ts
+++ src/errors/http.error.ts
@@ -1,9 +1,12 @@
-export class HttpError extends Error {
+import { HttpErrorInterface } from '@app/errors';
+
+export class HttpError extends Error implements HttpErrorInterface {
     statusCode: number;
+    message: string;
     context?: string;
 
     constructor(statusCode: number, message: string, context?: string) {
-        super();
+        super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.context = context;
