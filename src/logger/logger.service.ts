261d65c643c5eee542e41383563b745f90e47a0f
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-12 11:50:45 +0300

    fix imports: LoggerService

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,7 +1,7 @@
 import { injectable } from 'inversify';
 import { Logger } from 'tslog';
 
-import { ILogger } from './logger.interface';
+import { ILogger } from '@app/logger';
 
 @injectable()
 export class LoggerService implements ILogger {
