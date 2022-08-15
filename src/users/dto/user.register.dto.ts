21be850992415d6b4d972ecbdf377a4e35c73b83
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-15 13:38:34 +0300

    UserRegisterDto: a little move fields

--- src/users/dto/user.register.dto.ts
+++ src/users/dto/user.register.dto.ts
@@ -1,12 +1,12 @@
 import { IsEmail, IsString } from 'class-validator';
 
 export class UserRegisterDto {
+    @IsString({ message: 'Wrong name' })
+    name: string;
+
     @IsEmail({}, { message: 'Wrong email' })
     email: string;
 
     @IsString({ message: 'Wrong password' })
     password: string;
-
-    @IsString({ message: 'Wrong name' })
-    name: string;
 }
