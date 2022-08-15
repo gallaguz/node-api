99f478baf7a82c8a7df551bcffc516822e4cb629
Andrii Hallahuz <gallaguz@gmail.com>
2022-08-15 13:37:37 +0300

    IUserController: rename "register" => "registration"

--- src/users/user.controller.interface.ts
+++ src/users/user.controller.interface.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 
 export interface IUserController {
     login: (req: Request, res: Response, next: NextFunction) => void;
-    register: (req: Request, res: Response, next: NextFunction) => void;
+    registration: (req: Request, res: Response, next: NextFunction) => void;
     info: (req: Request, res: Response, next: NextFunction) => void;
 }
