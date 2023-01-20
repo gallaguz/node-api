63dfa74371953716c3c87195687841ceded304b4
gallaguz <gallaguz@gmail.com>
2023-01-21 02:33:18 +0300

    remove compiled files (mistake)

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    verbose: true,
-    preset: 'ts-jest',
-};
-exports.default = config;
-//# sourceMappingURL=jest.config.js.map
--- jest.e2e.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    verbose: true,
-    preset: 'ts-jest',
-    testRegex: '.e2e-spec.ts$',
-};
-exports.default = config;
-//# sourceMappingURL=jest.e2e.config.js.map
