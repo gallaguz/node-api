be5fe957a6856e0112af1e63be5c6168f27432cb
Andrii Hallahuz <gallaguz@gmail.com>
2023-09-08 04:06:00 +0300

    fix migrations/seed

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,72 +1,72 @@
-// import { PrismaClient, Prisma } from '@prisma/client'
-//
-// const prisma = new PrismaClient()
-//
-// const userData: Prisma.UserCreateInput[] = [
-//     {
-//         name: 'Alice',
-//         email: 'alice@prisma.io',
-//         posts: {
-//             create: [
-//                 {
-//                     title: 'Join the Prisma Slack',
-//                     content: 'https://slack.prisma.io',
-//                     published: true,
-//                 },
-//             ],
-//         },
-//     },
-//     {
-//         name: 'Nilu',
-//         email: 'nilu@prisma.io',
-//         posts: {
-//             create: [
-//                 {
-//                     title: 'Follow Prisma on Twitter',
-//                     content: 'https://www.twitter.com/prisma',
-//                     published: true,
-//                     viewCount: 42,
-//                 },
-//             ],
-//         },
-//     },
-//     {
-//         name: 'Mahmoud',
-//         email: 'mahmoud@prisma.io',
-//         posts: {
-//             create: [
-//                 {
-//                     title: 'Ask a question about Prisma on GitHub',
-//                     content: 'https://www.github.com/prisma/prisma/discussions',
-//                     published: true,
-//                     viewCount: 128,
-//                 },
-//                 {
-//                     title: 'Prisma on YouTube',
-//                     content: 'https://pris.ly/youtube',
-//                 },
-//             ],
-//         },
-//     },
-// ]
-//
-// async function main() {
-//     console.log(`Start seeding ...`)
-//     for (const u of userData) {
-//         const user = await prisma.user.create({
-//             data: u,
-//         })
-//         console.log(`Created user with id: ${user.id}`)
-//     }
-//     console.log(`Seeding finished.`)
-// }
-//
-// main()
-//     .then(async () => {
-//         await prisma.$disconnect()
-//     })
-//     .catch(async (e) => {
-//         console.error(e)
-//         await prisma.$disconnect()
-//         process.exit(1)
-//     })
+import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+const salt = Number(process.env.SALT);
+
+const randomUser = (): Record<string, string> => {
+    const name = faker.person.fullName();
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    return { name, email, password };
+};
+
+const getUsers = async (): Promise<Array<Record<string, string>>> => {
+    const userData: Array<Record<string, string>> = [
+        {
+            name: 'admin',
+            email: 'admin@admin.com',
+            password: await hash('admin', salt),
+        },
+        {
+            name: 'name',
+            email: 'email@email.com',
+            password: await hash('password', salt),
+        },
+        {
+            name: 'test',
+            email: 'test@test.com',
+            password: await hash('test', salt),
+        },
+    ];
+
+    for (let i = 0; i < 10; i++) {
+        userData.push(randomUser());
+    }
+
+    return userData;
+};
+
+async function main(): Promise<void> {
+    console.log(`--- Start seeding ...`);
+
+    const userData = await getUsers();
+
+    for (const { name, email, password } of userData) {
+        const user = await prisma.user.upsert({
+            where: { email },
+            update: {},
+            create: {
+                name,
+                email,
+                password,
+            },
+        });
+
+        console.log(`--- Created user with id: ${user.id}`);
+    }
+
+    console.log(`--- Seeding finished.`);
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
