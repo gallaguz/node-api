cfb84a99fcf3cd985251e3669861a51efa8b404c
Andrii Hallahuz <gallaguz@gmail.com>
2023-09-18 03:42:55 +0300

    feat: add errors

---
+++ src/errors/not-found.error.ts
@@ -0,0 +1,18 @@
+import {
+    CLIENT_ERROR_CODES,
+    STATUS_CODES_MESSAGES_MAP,
+} from '@app/constants/status.codes';
+
+export class NotFoundError extends Error {
+    statusCode: number;
+    message: string;
+    context?: string;
+
+    constructor(message?: string, context?: string) {
+        super(message);
+        this.statusCode = CLIENT_ERROR_CODES.NOT_FOUND;
+        this.message =
+            message || STATUS_CODES_MESSAGES_MAP[CLIENT_ERROR_CODES.NOT_FOUND];
+        this.context = context;
+    }
+}
---
+++ src/errors/unauthorized.error.ts
@@ -0,0 +1,19 @@
+import {
+    CLIENT_ERROR_CODES,
+    STATUS_CODES_MESSAGES_MAP,
+} from '@app/constants/status.codes';
+
+export class UnauthorizedError extends Error {
+    statusCode: number;
+    message: string;
+    context?: string;
+
+    constructor(message?: string, context?: string) {
+        super(message);
+        this.statusCode = CLIENT_ERROR_CODES.UNAUTHORIZED;
+        this.message =
+            message ||
+            STATUS_CODES_MESSAGES_MAP[CLIENT_ERROR_CODES.UNAUTHORIZED];
+        this.context = context;
+    }
+}
